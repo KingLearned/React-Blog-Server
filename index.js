af1b23e1c3e3f4ab47fb0439dc8575bd31c00d53
kinglearned <kinglearned@gmail.com>
2023-07-31 08:28:14 +0100

    Modifying the server api

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from 'cors'
 import authRoutes from './Routes/Auth.js'
 import postRoutes from './Routes/Posts.js'
 import MULTER from 'multer'
+const PORT = 1000
+
 const app = express()
 
 
@@ -13,8 +15,8 @@ app.use(cors({
     origin: '*'
 }));
 
-app.use("/server/auth", authRoutes)
 app.use("/server/posts", postRoutes)
+app.use("/server/auth", authRoutes)
 
 const newsImgStorage = MULTER.diskStorage({
     destination: '../Client/public/uploads',
@@ -24,38 +26,35 @@ const newsImgStorage = MULTER.diskStorage({
     }
 })
 
-const upload = MULTER({ storage:newsImgStorage })
+// const upload = MULTER({ storage:newsImgStorage })
 
-app.post('/server/upload', upload.single('file'), function (req, res){
-    if(req.file){
-        const newsImg = req.file
-        res.status(200).json(newsImg.filename)
-    } 
+// app.post('/server/upload', upload.single('file'), function (req, res){
+//     if(req.file){
+//         const newsImg = req.file
+//         res.status(200).json(newsImg.filename)
+//     } 
+// })
+
+// const ImgCache = MULTER.diskStorage({
+//     destination: '../../React-Blog-Project/Client/public/cacheImages',
+
+//     filename(req, file, cb){
+//         cb(null, file.originalname)
+//     }
+// })
+
+// const cacheImg = MULTER({ storage:ImgCache })
+
+// app.post('/server/imgpreview', cacheImg.single('cache'), function (req, res){
+//     if(req.file){
+//         const Img = req.file
+//         console.log(Img)
+//         res.status(200).json(Img.filename)
+//     } 
+// })
+
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`)
 })
 
-const ImgCache = MULTER.diskStorage({
-    destination: '../../React-Blog-Project/Client/public/cacheImages',
 
-    filename(req, file, cb){
-        cb(null, file.originalname)
-    }
-})
-
-const cacheImg = MULTER({ storage:ImgCache })
-
-app.post('/server/imgpreview', cacheImg.single('cache'), function (req, res){
-    if(req.file){
-        const Img = req.file
-        console.log(Img)
-        res.status(200).json(Img.filename)
-    } 
-})
-
-app.get('/', (req,res) => {
-    res.send('Hello World')
-})
-
-
-app.listen(8800, () => {
-    console.log('Server Running')
-})
